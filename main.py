ff3ea5ecda05b4d4c7053c605c688a318091a556
john <john@jlny.org>
2022-10-20 17:36:31 -0400

    Updated User references to Employee

--- main.py
+++ main.py
@@ -1,100 +1,107 @@
 # John Logiudice
 # INF 308 - Fall 2022
-# Assignment 8 - Classes and Objects
-# A user list/information manager
+# Assignment 9 - Inheritance
+# An employee list/information manager
 
-# Class to create user objects
-class User:
+# Class to create employee objects
+class Employee:
 
-    # Initialize a user object. Attributes stored in a dictionary. Name is required.
-    def __init__(self, user_name):
-        self.user_details = {'user': user_name,
-                             'name_first': None,
-                             'name_last': None,
-                             'eyes': None,
-                             'hair': None,
-                             'city': None,
-                             'state': None
-                             }
+    # Initialize an employee object. Attributes stored in a dictionary. Employee_id is required.
+    def __init__(self, employee_id):
+        self.employee_details = {'employee_id': employee_id,
+                                 'name_first': None,
+                                 'name_last': None,
+                                 'start_year': None,
+                                 'address': None,
+                                 'city': None,
+                                 'state': None
+                                 }
 
-    # Set user details. Update dictionary.
-    def set_user_name(self, user_name):
-        self.user_details['user'] = user_name
+    # Set employee details. Update dictionary.
+    def set_employee_id(self, employee_id):
+        self.employee_details['employee_id'] = employee_id
 
     def set_name_first(self, name_first):
-        self.user_details['name_first'] = name_first
+        self.employee_details['name_first'] = name_first
 
     def set_name_last(self, name_last):
-        self.user_details['name_last'] = name_last
+        self.employee_details['name_last'] = name_last
 
-    def set_eyes(self, eyes):
-        self.user_details['eyes'] = eyes
+    def set_start_year(self, start_year):
+        self.employee_details['start_year'] = start_year
 
-    def set_hair(self, hair):
-        self.user_details['hair'] = hair
+    def set_address(self, address):
+        self.employee_details['address'] = address
 
     def set_city(self, city):
-        self.user_details['city'] = city
+        self.employee_details['city'] = city
 
     def set_state(self, state):
-        self.user_details['state'] = state
+        self.employee_details['state'] = state
 
-    # Update all user details at once.
+    # Update all employee details at once.
     def add_details(self, details):
-        self.set_user_name(details[0])
+        self.set_employee_id(details[0])
         self.set_name_first(details[1])
         self.set_name_last(details[2])
-        self.set_eyes(details[3])
-        self.set_hair(details[4])
+        self.set_start_year(details[3])
+        self.set_address(details[4])
         self.set_city(details[5])
         self.set_state(details[6])
 
     # Return location - City & State
     def get_location(self):
-        return f"{self.user_details['city']} {self.user_details['state']}"
+        return f"{self.employee_details['city']} {self.employee_details['state']}"
 
     # Return full name - First & Last
     def get_full_name(self):
-        return f"{self.user_details['name_first']} {self.user_details['name_last']}"
+        return f"{self.employee_details['name_first']} {self.employee_details['name_last']}"
 
-    # Formatted user details for print to screen
+    # Formatted employee details for print to screen
     def show_details(self):
-        print(f"User: {self.user_details['user']}")
+        print(f"Employee: {self.employee_details['employee_id']}")
         print(f"Name: {self.get_full_name()}")
-        print(f"Eyes: {self.user_details['eyes']}")
-        print(f"Hair: {self.user_details['hair']}")
+        print(f"Start Year: {self.employee_details['start_year']}")
+        print(f"Address: {self.employee_details['address']}")
         print(f"Location: {self.get_location()}")
 
 
-# Class to create User List objects
-class UserList:
+class Manager(Employee):
+    def __init__(self, employee_id):
+        self.title = None
+        self.sub_list = None
+        super().__init__(self)
 
-    # Initialize the user list as an empty list
+
+# Class to create employee List objects
+class EmployeeList:
+
+    # Initialize the employee list as an empty list
     def __init__(self):
-        self.user_list = list()
+        self.employee_list = list()
 
-    # Add a new user object and add to the user list
-    def add_user(self, name, details):
-        # Add the user to the user list and create a new user object
-        self.user_list.append({'name': name, 'details': User(name)})
-        # For the latest user in the user list, add the acquired details
-        self.user_list[-1]['details'].add_details(details)
+    # Add a new employee object and add to the employee list
+    def add_employee(self, name, details):
+        # Add the employee to the employee list and create a new employee object
+        self.employee_list.append({'name': name, 'details': Employee(name)})
+        # For the latest employee in the employee list, add the acquired details
+        self.employee_list[-1]['details'].add_details(details)
 
-    # Find the user's index number by name
-    def find_user(self, name):
-        for i in range(0, len(self.user_list)):
-            if self.user_list[i]['name'].lower() == name.lower():
+    # Find the employee's index number by name
+    def find_employee(self, name):
+        for i in range(0, len(self.employee_list)):
+            if self.employee_list[i]['name'].lower() == name.lower():
                 return i
         return None
 
-    # sort the user list by user name
-    def sort_user_list(self):
-        self.user_list.sort(key=lambda name: name['name'])
+    # sort the employee list by employee ID
+    def sort_employee_list(self):
+        self.employee_list.sort(key=lambda name: name['name'])
 
-    # Get list of user names
-    def get_user_list(self):
-        # Get user list with list comprehension
-        return [user['name'] for user in self.user_list]
+    # Get list of employee IDs
+    def get_employee_list(self):
+        # Get employee list with list comprehension
+        return [employee['name'] for employee in self.employee_list]
 
 
 # Class to create a menu with menu numbers
@@ -129,22 +136,22 @@ class PickMenu:
             print()
 
 
-# Function to get the user name
+# Function to get the employee ID
 def get_name():
     # Loop until a name is entered
     while True:
-        name = input("Enter a new user name: ").strip()
+        name = input("Enter a new employee ID: ").strip()
         # Validate that text was entered
         if len(name) > 0:
             return name
         else:
-            print("The user name cannot be blank.")
+            print("The employee ID cannot be blank.")
             print()
 
 
-# Function to get new user details from user
+# Function to get new employee details from user
 def get_details(name):
-    # Get user name
+    # Get employee's name
     print("Name details")
     first = input("Enter the first name: ").strip()
     last = input("Enter the last name: ").strip()
@@ -152,8 +159,8 @@ def get_details(name):
 
     # Get personal details
     print("Personal description")
-    eyes = input("Enter the eye color: ").strip()
-    hair = input("Enter the hair color: ").strip()
+    start_year = input("Enter the start year: ").strip()
+    address = input("Enter the address: ").strip()
     print()
 
     # Get location details
@@ -162,25 +169,24 @@ def get_details(name):
     state = input("Enter the state abbreviation: ").strip()
     print()
 
-    return name, first, last, eyes, hair, city, state
+    return name, first, last, start_year, address, city, state
 
 
 # Function to execute user choice
-def menu_action(menu_choice, user_list):
-
-    # 1. Add a new user
+def menu_action(menu_choice, employee_list):
+    # 1. Add a new employee
     if menu_choice == 1:
-        add_user(user_list, get_name())
+        add_employee(employee_list, get_name())
         print()
 
-    # 2. Show user information
+    # 2. Show employee information
     elif menu_choice == 2:
-        show_user_list(user_list)
+        show_employee_list(employee_list)
         print()
 
-    # 3. Sort user list
+    # 3. Sort employee list
     elif menu_choice == 3:
-        sort_user_list(user_list)
+        sort_employee_list(employee_list)
         print()
 
     # 4. Quit
@@ -188,42 +194,42 @@ def menu_action(menu_choice, user_list):
         app_quit()
 
 
-# Function to add a new user
-def add_user(user_list_obj, name):
-    # Check if name is already in the user list
-    if not user_list_obj.find_user(name.lower()) is None:
-        # User name already exists
+# Function to add a new employee
+def add_employee(employee_list_obj, name):
+    # Check if name is already in the employee list
+    if not employee_list_obj.find_employee(name.lower()) is None:
+        # Employee ID already exists
         print('Sorry, that name is already taken.')
     else:
-        # User name is available
-        user_list_obj.add_user(name, get_details(name))
+        # Employee ID is available
+        employee_list_obj.add_employee(name, get_details(name))
 
 
-# Function to show the user list as a menu and show user details
-def show_user_list(user_list_obj):
-    # User List menu details
-    # Get list of user names from user object
-    user_menu_list = user_list_obj.get_user_list()
-    user_menu_name = 'User List'
-    user_menu_details = {'title': user_menu_name, 'items': user_menu_list}
+# Function to show the employee list as a menu and show employee details
+def show_employee_list(employee_list_obj):
+    # Employee List menu details
+    # Get list of employee IDs from employee object
+    employee_menu_list = employee_list_obj.get_employee_list()
+    employee_menu_name = 'Employee List'
+    employee_menu_details = {'title': employee_menu_name, 'items': employee_menu_list}
 
-    # Create the User List menu object
-    user_menu = PickMenu(user_menu_details)
+    # Create the Employee List menu object
+    employee_menu = PickMenu(employee_menu_details)
 
-    # Show the User List menu
-    user_menu.show_menu()
+    # Show the Employee List menu
+    employee_menu.show_menu()
 
-    # Get the user menu choice - subtract 1 to match list index
-    user_choice = user_menu.get_menu_response() - 1
+    # Get the employee menu choice - subtract 1 to match list index
+    employee_choice = employee_menu.get_menu_response() - 1
     print()
 
-    # call the show user details method
-    user_list_obj.user_list[user_choice]['details'].show_details()
+    # Call the show employee details method
+    employee_list_obj.employee_list[employee_choice]['details'].show_details()
 
 
-# Function to call the user list sort
-def sort_user_list(user_list_obj):
-    user_list_obj.sort_user_list()
+# Function to call the employee list sort
+def sort_employee_list(employee_list_obj):
+    employee_list_obj.sort_employee_list()
 
 
 # Function to quit the program
@@ -233,16 +239,19 @@ def app_quit():
 
 
 def main():
-    # Create the User List object
-    user_list = UserList()
+    # Create the Employee List object
+    employee_list = EmployeeList()
 
-    # Sample user list Data
-    user_list.add_user('John23', ('John23', 'John', 'Logiudice', 'brown', 'brown', 'Wala Wala', 'WA'))
-    user_list.add_user('RollyPanda', ('RollyPanda', 'Gandolph', 'Verspasian', 'hazel', 'blonde', 'Portland', 'OR'))
-    user_list.add_user('Jandra2001', ('Jandra2001', 'Jandra', 'De La Cruz', 'green', 'brown', 'La Jolla', 'CA'))
+    # Sample employee list Data
+    employee_list.add_employee('ID289435',
+                               ('ID289435', 'John', 'Logiudice', '2015', '134 Market Square', 'Wala Wala', 'WA'))
+    employee_list.add_employee('ID279423', (
+        'ID279423', 'Gandolph', 'Verspasian', '2018', '200-20 3rd Ave Apt 12B', 'Portland', 'OR'))
+    employee_list.add_employee('ID280121',
+                               ('ID280121', 'Jandra', 'De La Cruz', '2021', '74 Carlos Silva Dr.', 'La Jolla', 'CA'))
 
     # Main Menu details
-    main_menu_tuple = ('Add a new user', 'Show user information', 'Sort user list', 'Quit')
+    main_menu_tuple = ('Add a new employee', 'Show employee information', 'Sort employee list', 'Quit')
     main_menu_name = 'Main Menu'
     main_menu_details = {'title': main_menu_name, 'items': main_menu_tuple}
 
@@ -260,7 +269,7 @@ def main():
         print()
 
         # Execute user menu choice
-        menu_action(user_choice, user_list)
+        menu_action(user_choice, employee_list)
 
 
 # Execute the main function
